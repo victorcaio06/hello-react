7294951234e94c2f8393953cf92d1e8993d194b6
Victor Caio <kaioaraujo426@gmail.com>
2022-03-22 23:52:44 -0300

    hello world

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import './Hello.css';
+
+function HelloWorld(){
+    return (
+        <div className="Hello">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
